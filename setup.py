462c69541dbf4bfa394b991c194e071a1c9e58a0
Michael Mulich <michael.mulich@gmail.com>
2016-09-24 15:37:53 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='icanhaz',
-    version='1.0.1',
+    version='1.1.0',
     author='Michael Mulich',
     license='Public Domain',
     description="I Can Haz GitFlow?",
