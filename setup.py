fbbed63636ede9463b4fc7e086f5b6ec646cbef0
Michael Mulich <michael.mulich@gmail.com>
2016-09-24 15:58:03 -0700

    Bump major version v2.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='icanhaz',
-    version='1.1.0',
+    version='2.0.0',
     author='Michael Mulich',
     license='Public Domain',
     description="I Can Haz GitFlow?",
